59f5a977578125ce1cb1c45d79d073b43f1eae08
Georgiana Ogrean <georgiana.ogrean@gmail.com>
2020-02-06 16:03:07 -0800

    Tract boundaries now intersect with county boundaries to accurately map the boundaries near the edge of a county.

--- code/assign_tracts.py
+++ code/assign_tracts.py
@@ -7,6 +7,7 @@ from shapely.geometry import Point, Polygon
 
 DATA_DIR = '/Users/gogrean/Documents/data_projects/Fire_Department_Calls/data/'
 
+
 class CountyShape:
     """Get county boundaries.
 
@@ -34,7 +35,9 @@ class Tracts:
     The class loads the tract data and converts the boundaries of each
     tract into a `shapely` polygon."""
 
-    def __init__(self, tracts_filename='Census_2010_Tracts.csv'):
+    def __init__(self,
+                 county='San Francisco',
+                 tracts_filename='Census_2010_Tracts.csv'):
         """
         Read the US Census tract data.
         """
@@ -43,17 +46,18 @@ class Tracts:
                                          'AWATER10', 'NAMELSAD10'])
         tracts_df['GEOID10'] = tracts_df['GEOID10'].astype('str')
         self.df = tracts_df
+        self.county = county
+        self.longitude, self.latitude = [], []
+        self.tract_names = []
 
     def _convert_boundary_to_shapely_polygon(self):
-        """
-        Convert boundary coordinates to `shapely` POLYGON.
+        """Convert boundary coordinates to `shapely` POLYGON.
 
         In the tract dataframe, create a column the defines the boundary of
         each tract in a 'shapely' POLYGON format.
 
         The tract dataframe must have a 'the_geom' column containing the
-        coordinates of the polygon boundaries.
-        """
+        coordinates of the polygon boundaries."""
         start, end = 'MULTIPOLYGON (((', ')))'
 
         for i, tract in enumerate(self.df['the_geom']):
@@ -66,33 +70,52 @@ class Tracts:
         self.df.drop(labels='the_geom', axis=1, inplace=True)
 
     def get_boundaries(self):
+        """Intersect the county boundary with the tract polygons.
+
+        The tract polygons have rough edges and do not presisely follow the
+        contours of the county's boundary. Intersecting the tracts with the
+        county shape ensures a correct mapping of each tract's land area.
+        """
         self._convert_boundary_to_shapely_polygon()
+        county = CountyShape(self.county)
         # TODO: Would it make more sense to index this by tract id?
         for i in self.df.index:
-            # If the tract has no land area...
-            if not self.df.at[i, 'ALAND10']:
-                self.df.loc[i, 'X'] = []
-                self.df.loc[i, 'Y'] = []
-            else:
-                # This is the land area of (most of) San Francisco County. Apparently also included
-                # in SF County are a few islands around the continental part? Those are mapped in
-                # sf.values[0][1:19]. However, I will ignore those areas here.
-                land_poly = sf_tracts_df.at[i, 'Polygon'].intersection(sf.values[0][0])
+            tract_id = self.df.at[i, 'GEOID10']
+            # If the tract has any land area...
+            if self.df.at[i, 'ALAND10']:
+                # This is the land area of (most of) San Francisco County.
+                # Also included in SF County are a few islands around the
+                # continental part? Those are mapped in sf.values[0][1:19].
+                # I will ignore those areas for now, hence the use of
+                # county.boundary.values[0][0].
+                # TODO: Handle multiple boundary polygons.
+                land_poly = self.df.at[i, 'Polygon'].intersection(
+                            county.boundary.values[0][0])
                 try:
-                    tract_coords_xy = [(x, y) for x, y in land_poly.exterior.coords]
+                    tract_coords_xy = [(x, y)
+                                       for x, y in land_poly.exterior.coords]
+                # AttributeError is thrown if the intersection between
+                # self.df.at[i, 'Polygon'] and county.boundary.values[0][0]
+                # results in unconnected polygons. The individual polygons
+                # are then handled separately, making sure that the same tract
+                # number is assigned to all of them.
                 except AttributeError:
                     multi_land_polys = list(land_poly)
                     n_polys = len(multi_land_polys)
                     for p in multi_land_polys:
                         tract_coords_xy = [(x, y) for x, y in p.exterior.coords]
-                        tract_coords_x.append([x for x, _ in tract_coords_xy])
-                        tract_coords_y.append([y for _, y in tract_coords_xy])
+                        self.longitude.append(
+                                                [x for x, _ in tract_coords_xy])
+                        self.latitude.append(
+                                                [y for _, y in tract_coords_xy])
                 else:
+                # This else is here just to avoid having all this code under
+                # the `try` statement.
                     n_polys = 1
-                    tract_coords_x.append([x for x, _ in tract_coords_xy])
-                    tract_coords_y.append([y for _, y in tract_coords_xy])
+                    self.longitude.append([x for x, _ in tract_coords_xy])
+                    self.latitude.append([y for _, y in tract_coords_xy])
                 finally:
-                    tract_names.extend([tract_id]*n_polys)
+                    self.tract_names.extend([tract_id]*n_polys)
 
 
 class MedicalIncidents():
@@ -160,7 +183,7 @@ class MedicalIncidents():
     def _get_tract_data(tracts_filename):
         """Make Tracts() object, with the boundaries as `shapely` polygons."""
         tracts = Tracts(tracts_filename=tracts_filename)
-        tracts._convert_boundary_to_shapely_polygon()
+        tracts.get_boundaries()
         return tracts
 
     @staticmethod
