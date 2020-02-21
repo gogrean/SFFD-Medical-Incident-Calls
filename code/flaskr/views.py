3e5388dfd87fbe7cfe081fb35c34973a7cfb5eaf
Georgiana Ogrean <georgiana.ogrean@gmail.com>
2020-02-21 13:13:23 -0800

    The city and state of the dispatcher is now automatically detected (find_me function in location_utils.py), so that the dispatcher does not need to type in that information every single time; that info does not change.

--- code/flaskr/views.py
+++ code/flaskr/views.py
@@ -3,16 +3,32 @@ import datetime as dt
 from flask import render_template, request
 
 from . import app
+from code.location_tools import find_me, add_city_state
 
 @app.route('/')
 def index():
     """Homepage."""
+
     return render_template("homepage.html")
 
 @app.route('/prediction')
 def estimated_wait_time():
     current_DtTm = dt.datetime.now()
+    street_address = request.args['incident-address']
     priority = request.args['priority']
     unit_type = request.args['unit-type']
-    print("SANITY CHECK: ", current_DtTm, priority, unit_type)
-    return "Hey, this works."
+
+    dispatch_loc = find_me()
+    address = add_city_state(
+        street_address,
+        dispatch_loc['city'],
+        dispatch_loc['state'],
+    )
+
+    return (
+        "SANITY CHECK:"
+        f"\t Address: {address}, "
+        f"\t Current DtTm: {current_DtTm}, "
+        f"\t Priority: {priority}, "
+        f"\t Unit Type: {unit_type}"
+    )
--- code/location_tools.py
+++ code/location_tools.py
@@ -1,14 +1,29 @@
 from geopy.geocoders import Nominatim
+import geocoder
 
 from code.exceptions import AddressError
 
 
 def decode_address(address):
     """Geocode address."""
-
     geolocator = Nominatim(user_agent="my_fd_app")
 
     return geolocator.geocode(address, timeout=180)
+
+
+def find_me():
+    """Get geographical coordinates, city, and state based on user IP."""
+    myloc = geocoder.ip('me')
+    myloc_properties = myloc.geojson['features'][0]['properties']
+    return {
+        'city': myloc_properties['city'],
+        'state': myloc_properties['state'],
+        'latitude': myloc_properties['lat'],
+        'longitude': myloc_properties['lng'],
+    }
+
+def add_city_state(address, city, state):
+    return address + f", {city} {state}"
 
 
 def get_coords_from_address(street_address, city=None, state=None):
@@ -17,12 +32,9 @@ def get_coords_from_address(street_address, city=None, state=None):
     Hacky way of deciphering an address and converting it to a GeoPy location
     with longitude and latitude attributes."""
 
-    def add_city_state(address):
-        return address + f", {city} {state}"
-
     full_address = street_address
     if city and state:
-        full_address = add_city_state(street_address)
+        full_address = add_city_state(street_address, city, state)
 
     location = decode_address(full_address)
     if location:
@@ -31,7 +43,11 @@ def get_coords_from_address(street_address, city=None, state=None):
     # if address not found on the first try
     for street_type in ['Street', 'Avenue', 'Road', 'Court', 'Way']:
         if city and state:
-            full_address = add_city_state(f"{street_address} {street_type}")
+            full_address = add_city_state(
+                f"{street_address} {street_type}",
+                city,
+                state
+            )
         else:
             full_address = street_address.split(",", 1)[0] + \
                            f" {street_type}, " + street_address.split(",", 1)[1]
