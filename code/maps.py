222c0db30b5e9755c7d79046f210b3868b069631
Georgiana Ogrean <georgiana.ogrean@gmail.com>
2020-03-04 17:00:32 -0800

    Added static method to create a dictionary with the necessary tract info. This allows accessing the info without constantly querying the database and handling PostGIS objects.

--- code/maps.py
+++ code/maps.py
@@ -41,3 +41,59 @@ class MapPlotter:
         ).scalar()
 
         self.data = {}
+
+    @staticmethod
+    def _build_tract_dict():
+        all_tracts = [
+            tr[0] for tr in db.session.query(
+                                TractGeometry.geoid10
+                            ).all()
+        ]
+
+        all_tract_land_areas = [
+            tr[0] for tr in db.session.query(
+                                TractGeometry.aland10
+                            ).all()
+        ]
+
+        all_tract_geoms = [
+            tr[0] for tr in db.session.query(
+                            TractGeometry.the_geom
+                        ).all()
+        ]
+
+        all_tract_lng = []
+        all_tract_lat = []
+        for tract_geom in all_tract_geoms:
+            # a non-Pythonic way to check for null geometries, since objects
+            # of type WKBElement do not support boolean operations
+            if tract_geom is None:
+                continue
+            tmp_lng, tmp_lat = [], []
+            for pg in list(to_shape(tract_geom).geoms):
+                c = pg.exterior.coords.xy
+                tmp_lng.append(list(c[0]))
+                tmp_lat.append(list(c[1]))
+            all_tract_lng.append(tmp_lng)
+            all_tract_lat.append(tmp_lat)
+
+        return {tract_id: {
+                    'aland10': aland10,
+                    'lng': lng,
+                    'lat': lat,
+                }
+                for tract_id, aland10, lng, lat in zip(
+                    all_tracts,
+                    all_tract_land_areas,
+                    all_tract_lng,
+                    all_tract_lat,
+                )
+        }
+
+    def make_maps(self):
+        """Generate maps of response time and number of incidents."""
+
+        tract_dict = self._build_tract_dict()
+
+        for yr in range(self.min_year, self.max_year + 1):
+            break
