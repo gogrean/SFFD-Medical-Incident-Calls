b4c05409a5b55c753cea934b2d4ec626278b0e9b
Georgiana Ogrean <georgiana.ogrean@gmail.com>
2020-03-10 14:45:00 -0700

    Modified the key reader utility function to navigate to the credentials directory when the code is being run from a different location than the 'code' directory.

--- code/key_utils.py
+++ code/key_utils.py
@@ -1,7 +1,16 @@
 import yaml
+import os
 
-def get_secret_key(key_name, key_fp="../credentials/keys.yml"):
+
+def get_secret_key(key_name, key_fp=None):
     """Read a key from a YAML file."""
+
+    # If the filepath for the key file is not given, navigate to
+    # the ROOT_DIR/credentials/keys.yml. And cross your fingers...
+    if not key_fp:
+        file_dir = os.path.dirname(os.path.realpath(__file__))
+        key_fp = "/".join(file_dir.split('/')[:-1])
+        key_fp += "/credentials/keys.yml"
 
     with open(key_fp, 'r') as f:
         credentials = yaml.load(f, Loader=yaml.FullLoader)
