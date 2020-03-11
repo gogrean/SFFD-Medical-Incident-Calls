9bc454ee8d6111e7202d494bab5746ef4d6ede93
Georgiana Ogrean <georgiana.ogrean@gmail.com>
2020-03-10 17:38:51 -0700

    Moved the code that finds the credentials directory into a separate function.

--- code/key_utils.py
+++ code/key_utils.py
@@ -2,15 +2,21 @@ import yaml
 import os
 
 
+def _find_credentials_dir():
+    """Try to build the path to the '../credentials' directory."""
+    file_dir = os.path.dirname(os.path.realpath(__file__))
+    key_fp = "/".join(file_dir.split('/')[:-1])
+    key_fp += "/credentials/keys.yml"
+
+    return key_fp
+
 def get_secret_key(key_name, key_fp=None):
     """Read a key from a YAML file."""
 
     # If the filepath for the key file is not given, navigate to
     # the ROOT_DIR/credentials/keys.yml. And cross your fingers...
     if not key_fp:
-        file_dir = os.path.dirname(os.path.realpath(__file__))
-        key_fp = "/".join(file_dir.split('/')[:-1])
-        key_fp += "/credentials/keys.yml"
+        key_fp = _find_credentials_dir()
 
     with open(key_fp, 'r') as f:
         credentials = yaml.load(f, Loader=yaml.FullLoader)
